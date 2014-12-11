e004efe41dbca72634787a579274a8355a37582b
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-12-11 20:45:22 +0100

    feat(UI) : Add Bootstrap and jQuery 1.11 to the project

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,11 +8,10 @@ namespace SchoolManager
         // Pour plus d'informations sur Bundling, accédez à l'adresse http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-1.11.js", 
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap-js").Include(
-                "~/Scripts/bootstrap-min.js"));
+                        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -26,7 +25,8 @@ namespace SchoolManager
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/themes/bootstrap/bootstrap-min.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/themes/bootstrap/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
