b1cbeddeb77ae99427e0b91b197d8df98055425e
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-12-11 23:39:11 +0100

    feat(Administration): Add Index read operation for Years.

---
+++ Controllers/YearController.cs
@@ -0,0 +1,32 @@
+﻿using SchoolManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManager.Controllers
+{
+    public class YearController : Controller
+    {
+        //
+        // GET: /Year/
+
+        public ActionResult Index()
+        {
+            using (var entity = new Entities())
+            {
+                var repo = new YearRepository(entity);
+                List<YearModel> levels = repo.All().Select(y => new YearModel
+                    {
+                        Id = y.Id,
+                        Year1 = y.Year1
+
+                    }).OrderBy(y => y.Year1).ToList();
+
+                return View(levels);
+            }
+        }
+
+    }
+}
---
+++ Models/YearRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManager.Models
+{
+    public class YearRepository
+    {
+         public Entities context;
+
+        public YearRepository(Entities ent)
+        {
+            context = ent;
+        }
+
+        public IQueryable<Year> All() {
+            return context.Years;
+        }
+
+        public IQueryable<Year> getById(Guid id)
+        {
+            return context.Years.Where(s => s.Id == id);
+        }
+    }
+}
