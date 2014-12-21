01ee788e6d4917640719f7f4660ec6a6f78a32be
Jean-Loup Adde <jean-loup.adde@orange.fr>
2014-12-22 00:01:01 +0100

    feat(ClassManagement): Add Evaluation functionalities (Read + Edit Results)

--- Areas/ClassManagement/Controllers/EvaluationController.cs
+++ Areas/ClassManagement/Controllers/EvaluationController.cs
@@ -174,6 +174,131 @@ namespace SchoolManager.Areas.ClassManagement.Controllers
             return RedirectToAction("Read", results[0].Evaluation_Id);
         }
 
+        [HttpGet]
+        public ActionResult Read(Guid id)
+        {
+            using (var entity = new Entities())
+            {
+                ResultRepository resultRepo = new ResultRepository(entity);
+                EvaluationRepository evalRepo = new EvaluationRepository(entity);
+                EvaluationModel eval = evalRepo.getById(id).Select(e => new EvaluationModel
+                {
+                    Id = e.Id,
+                    Classroom_Id = e.Classroom_Id,
+                    Classroom_Title = e.Classroom.Title,
+                    User_Id = e.User_Id,
+                    User_Name = e.User.FirstName + " " + e.User.LastName,
+                    Period_Id = e.Period_Id,
+                    Period_Begin = e.Period.Begin,
+                    Period_End = e.Period.End,
+                    Year1 = e.Period.Year.Year1,
+                    Year_Id = e.Period.Year_Id,
+                    Date = e.Date,
+                    TotalPoint = e.TotalPoint,
+                    Results_Number = e.Results.Count
+                }).First();
+                ViewData["results"] = resultRepo.getByEvaluation(id).Select(r => new ResultModel
+                {
+                    Id = r.Id,
+                    Note = r.Note,
+                    Evaluation_Id = r.Evaluation.Id,
+                    EvaluationTotalPoint = r.Evaluation.TotalPoint,
+                    EvaluationDate = r.Evaluation.Date,
+                    Pupil_Id = r.Pupil_Id,
+                    Pupil_Name = r.Pupil.FirstName + " " + r.Pupil.LastName
+                }).ToList();
+                return View(eval);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Guid id)
+        {
+            using(var entity = new Entities()) {
+                EvaluationRepository evalRepo = new EvaluationRepository(entity);
+                ClassroomRepository classroomRepo = new ClassroomRepository(entity);
+                PeriodRepository periodRepo = new PeriodRepository(entity);
+                UserRepository userRepo = new UserRepository(entity);
+                ResultRepository resultRepo = new ResultRepository(entity);
+                EvaluationModel eval = evalRepo.getById(id).Select(e => new EvaluationModel
+                {
+                    Id = e.Id,
+                    Classroom_Id = e.Classroom_Id,
+                    Classroom_Title = e.Classroom.Title,
+                    User_Id = e.User_Id,
+                    User_Name = e.User.FirstName + " " + e.User.LastName,
+                    Period_Id = e.Period_Id,
+                    Period_Begin = e.Period.Begin,
+                    Period_End = e.Period.End,
+                    Year1 = e.Period.Year.Year1,
+                    Year_Id = e.Period.Year_Id,
+                    Date = e.Date,
+                    TotalPoint = e.TotalPoint,
+                    Results_Number = e.Results.Count
+                }).First();
+                ViewData["classrooms"] = classroomRepo.All().Select(c => new ClassroomModel
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                    User_Id = c.User.Id,
+                    User_Name = c.User.FirstName + " " + c.User.LastName,
+                    Year1 = c.Year.Year1,
+                    Establishment_Name = c.Establishment.Name
+                }).ToList();
+                ViewData["periods"] = periodRepo.All().Select(p => new PeriodModel
+                {
+                    Id = p.Id,
+                    Begin = p.Begin,
+                    End = p.End
+                }).ToList();
+                eval.Results = resultRepo.getByEvaluation(id).Select(r => new ResultModel {
+                    Id = r.Id,
+                    Note = r.Note,
+                    Evaluation_Id = r.Evaluation.Id,
+                    EvaluationTotalPoint = r.Evaluation.TotalPoint,
+                    EvaluationDate = r.Evaluation.Date,
+                    Pupil_Id = r.Pupil_Id,
+                    Pupil_Name = r.Pupil.FirstName + " " + r.Pupil.LastName
+                }).ToList();
+                return View(eval);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EvaluationModel eval) {
+            if (ModelState.IsValid)
+            {
+                using(var entity = new Entities()) {
+                EvaluationRepository evaluationRepo = new EvaluationRepository(entity);
+                ClassroomRepository classroomRepo = new ClassroomRepository(entity);
+                UserRepository userRepo = new UserRepository(entity);
+
+                eval.User_Id = classroomRepo.getById(eval.Classroom_Id).First().User_Id;
+                Evaluation e = evaluationRepo.getById(eval.Id).First();
+                convertEvaluationModelToEvaluation(eval, e);
+                evaluationRepo.Save();
+                return RedirectToAction("Read", new { @id = eval.Id });
+                }
+            }
+            return View(eval);
+        }
+
+        [HttpPost]
+        public ActionResult EditResults(List<ResultModel> results)
+        {
+            using (var entity = new Entities())
+            {
+                ResultRepository resultRepo = new ResultRepository(entity);
+                foreach (var result in results)
+                {
+                    Result r = resultRepo.getById(result.Id).First();
+                    convertResultModelToResult(result, r);
+                    resultRepo.Save();
+                }
+                return RedirectToAction("Read", new { @id = results[0].Evaluation_Id });
+            }
+        }
+
        
     }
 }
